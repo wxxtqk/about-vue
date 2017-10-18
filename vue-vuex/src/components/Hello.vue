9da898c557ab66d2907bf45ba3eabb1db069fb39
tangquankun <1072317596@qq.com>
2017-10-18 10:17:25 +0800

    module

--- vue-vuex/src/components/Hello.vue
+++ vue-vuex/src/components/Hello.vue
@@ -1,14 +1,34 @@
 <template>
   <div>
-  <h4>实现module</h4>
+  <h4>实现module功能</h4>
+  <button @click="add">增加</button>
+  <span>module中的count: {{sumCount}}</span><br>
+  <span>rootState中的count：{{rootStateCount}}</span><br>
+  <span>rootState和state中count相加{{sumCountAll}}</span>
   </div>
 </template>
 
 <script>
+import {mapState, mapGetters} from 'vuex'
 export default {
-  name: 'hello'
+  name: 'hello',
+  computed: {
+    ...mapState({
+      sumCount: state => state.a.count,
+      rootStateCount: rootState => rootState.a.count
+    }),
+    ...mapGetters({
+      sumCountAll: 'sumWithRootCount'
+    })
+  },
+  methods: {
+    add () {
+      this.$store.dispatch('incrementIfOddOnRootSum')
+    }
+  }
 }
 </script>
+
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
--- vue-vuex/src/store/bus.js
+++ vue-vuex/src/store/bus.js
@@ -2,8 +2,32 @@ import Vuex from 'vuex'
 import Vue from 'vue'
 Vue.use(Vuex)
 const moduleA = {
-	
+	state: {
+		count: 0
+	},
+	mutations: {
+		sumCount (state) {
+			state.count++
+		}
+	},
+	getters: {
+		doubleCount (state) {
+			return state.count * 2
+		},
+		sumWithRootCount (state, getters, rootState){
+			return state.count + rootState.a.count
+		}
+	},
+	actions: {
+		incrementIfOddOnRootSum ({ state, commit, rootState }) {
+				commit('sumCount')
+
+		}
+	}
 }
 const store = new Vuex.Store({
+	modules: {
+		a: moduleA
+	}
 })
 export default store
